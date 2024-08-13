93672a46b0609b013f5aee80e310fe0c86bec8d4
PRASANTH GANTEDA <prasanthganteda001@gmail.com>
2024-08-13 18:20:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+$(function() {
+    $('.page-scroll a').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+$('body').scrollspy({
+    target: '.navbar-fixed-top'
+})
